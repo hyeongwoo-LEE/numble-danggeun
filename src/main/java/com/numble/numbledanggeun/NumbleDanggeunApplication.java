cc69352e67bd6597c0b01b2eb34e230380ef18bc
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-01-12 17:13:35 +0900

    init

---
+++ src/main/java/com/numble/numbledanggeun/NumbleDanggeunApplication.java
@@ -0,0 +1,13 @@
+package com.numble.numbledanggeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleDanggeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbleDanggeunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numble/numbledanggeun/NumbleDanggeunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.numbledanggeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbleDanggeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
